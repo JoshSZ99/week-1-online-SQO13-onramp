10362e88cfd0f2e2716a71ec9c2fcbd3cee491b1
Olayinka Saka <105171883+JoshSZ99@users.noreply.github.com>
2022-09-13 01:12:03 +0100

    Update challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,7 +1,15 @@
 function fizzBuzz(input){
-
-  
-}
+    if(input % 3 === 0 && input % 5 === 0){
+      return "fizzbuzz";
+    } else if(input % 5 === 0){
+      return "buzz"
+    } else if (input % 3 === 0){
+      return "fizz";
+    } else{
+      return input;
+    }
+      
+    }
 
 
 module.exports = fizzBuzz
