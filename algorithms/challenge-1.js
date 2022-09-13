c42bfa395350eda516848ca1f523d7d31dccc810
Olayinka Saka <105171883+JoshSZ99@users.noreply.github.com>
2022-09-13 01:09:59 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,12 @@
 function countTruthy(arr) {
+  let countArr = 0;
 
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i])
+        countArr++;
+  }
+
+  return countArr;
 }
+
 module.exports = countTruthy
